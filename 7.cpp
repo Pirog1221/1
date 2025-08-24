802774fc048577359a56b9176662c8a52a877894
Ветчинкин Михаил ТСА-142 <142144452+Pirog1221@users.noreply.github.com>
2025-08-24 18:08:16 +0300

    Update 7.cpp

--- 7.cpp
+++ 7.cpp
@@ -1,85 +1,99 @@
 #include <iostream>
-#include <vector>
 #include <cstdlib>
 #include <ctime>
 #include <limits>
 
 using namespace std;
 /**
- * @brief Создает двумерный массив (вектор векторов) заданного размера
+ * @brief Создает двумерный динамический массив целых чисел
  * @param n Количество строк в массиве
  * @param m Количество столбцов в массиве
- * @return Двумерный вектор целых чисел размером n x m
- * @note Все элементы массива инициализируются нулями
+ * @return Указатель на созданный двумерный массив
+ * @note Память выделяется в куче, необходимо освобождение с помощью freeArray()
+ * @warning Все элементы массива инициализируются нулями
  */
-vector<vector<int>> createArray(int n, int m)
+int** createArray(int n, int m)
 
 /**
  * @brief Заполняет массив случайными целыми числами в диапазоне [0, 99]
- * @param arr Ссылка на двумерный вектор для заполнения
+ * @param arr Указатель на двумерный массив для заполнения
+ * @param n Количество строк в массиве
+ * @param m Количество столбцов в массиве
  * @note Использует текущее время как seed для генератора случайных чисел
  * @warning Вызывает srand(time(0)), что влияет на все последующие вызовы rand()
  */
-void fillRandom(vector<vector<int>>& arr) 
+void fillRandom(int** arr, int n, int m)
 
 /**
  * @brief Заполняет массив значениями, введенными пользователем с клавиатуры
- * @param arr Ссылка на двумерный вектор для заполнения
+ * @param arr Указатель на двумерный массив для заполнения
+ * @param n Количество строк в массиве
+ * @param m Количество столбцов в массиве
  * @note Запрашивает ввод для каждого элемента массива
  * @note Обрабатывает ошибки ввода, требуя корректные целые числа
  * @warning В случае ошибки ввода очищает флаги и буфер cin
  */
-void fillManual(vector<vector<int>>& arr) 
+void fillManual(int** arr, int n, int m)
 
 /**
  * @brief Выводит содержимое массива в табличном формате
- * @param arr Константная ссылка на двумерный вектор для вывода
+ * @param arr Указатель на двумерный массив для вывода
+ * @param n Количество строк в массиве
+ * @param m Количество столбцов в массиве
  * @note Элементы выводятся через табуляцию, каждая строка с новой линии
  * @note Форматирование сохраняет выравнивание столбцов
  */
-void printArray(const vector<vector<int>>& arr) 
+void printArray(int** arr, int n, int m)
 
 /**
  * @brief Заменяет максимальный элемент в каждой строке массива на ноль
- * @param arr Ссылка на двумерный вектор для модификации
+ * @param arr Указатель на двумерный массив для модификации
+ * @param n Количество строк в массиве
+ * @param m Количество столбцов в массиве
  * @note Для каждой строки находит максимальный элемент и устанавливает его в 0
  * @note Если в строке несколько максимальных элементов, заменяется первый из них
- * @uses algorithm::max_element для поиска максимального элемента
+ * @note Использует линейный поиск максимального элемента
  */
-void replaceMaxWithZero(vector<vector<int>>& arr) 
+void replaceMaxWithZero(int** arr, int n, int m) 
 
 /**
  * @brief Вставляет строки из нулей перед строками, где первый элемент делится на 3
- * @param arr Ссылка на двумерный вектор для модификации
- * @note Создает новый массив, вставляя перед подходящими строками строки нулей
- * @note Проверяет, что строка не пустая перед проверкой первого элемента
- * @note Размер вставляемой строки нулей соответствует размеру исходной строки
- * @return Модифицированный массив с добавленными строками нулей
+ * @param arr Указатель на двумерный массив для модификации
+ * @param n Ссылка на количество строк в массиве (изменяется при вставке)
+ * @param m Количество столбцов в массиве
+ * @return Указатель на новый массив с добавленными строками
+ * @note Выделяет новую память под массив с дополнительными строками
+ * @note Освобождает память старого массива
+ * @note Обновляет значение n до нового количества строк
+ * @warning Старый массив освобождается, нельзя использовать после вызова функции
  */
-void insertZeroRows(vector<vector<int>>& arr) 
+int** insertZeroRows(int** arr, int& n, int m)
 
 int main() {
     setlocale(LC_ALL, "Russian");
 // Функция для создания массива
-vector<vector<int>> createArray(int n, int m) {
-    return vector<vector<int>>(n, vector<int>(m));
-}
+int** createArray(int n, int m) {
+    int** arr = new int*[n];
+    for (int i = 0; i < n; ++i) {
+        arr[i] = new int[m]();
+    }
+    return arr;
 
 // Функция для заполнения массива случайными числами
-void fillRandom(vector<vector<int>>& arr) {
+void fillRandom(int** arr, int n, int m) {
     srand(time(0));
-    for (auto& row : arr) {
-        for (auto& elem : row) {
-            elem = rand() % 100; // числа от 0 до 99
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            arr[i][j] = rand() % 100;
         }
     }
 }
 
 // Функция для заполнения массива с клавиатуры
-void fillManual(vector<vector<int>>& arr) {
+void fillManual(int** arr, int n, int m) {
     cout << "Введите элементы массива:\n";
-    for (size_t i = 0; i < arr.size(); ++i) {
-        for (size_t j = 0; j < arr[i].size(); ++j) {
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
             cout << "Элемент [" << i << "][" << j << "]: ";
             while (!(cin >> arr[i][j])) {
                 cout << "Ошибка! Введите целое число: ";
@@ -91,35 +105,39 @@ void fillManual(vector<vector<int>>& arr) {
 }
 
 // Функция для вывода массива
-void printArray(const vector<vector<int>>& arr) {
+void printArray(int** arr, int n, int m) {
     cout << "Массив:\n";
-    for (const auto& row : arr) {
-        for (const auto& elem : row) {
-            cout << elem << "\t";
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            cout << arr[i][j] << "\t";
         }
         cout << "\n";
     }
 }
 
 // Функция для замены максимального элемента каждой строки нулем
-void replaceMaxWithZero(vector<vector<int>>& arr) {
-    for (auto& row : arr) {
-        auto max_it = max_element(row.begin(), row.end());
-        *max_it = 0;
+void replaceMaxWithZero(int** arr, int n, int m) {
+    for (int i = 0; i < n; ++i) {
+        int max_index = 0;
+        for (int j = 1; j < m; ++j) {
+            if (arr[i][j] > arr[i][max_index]) {
+                max_index = j;
+            }
+        }
+        arr[i][max_index] = 0;
     }
 }
 
+
 // Функция для вставки строк из нулей перед строками, где первый элемент делится на 3
-void insertZeroRows(vector<vector<int>>& arr) {
-    vector<vector<int>> newArr;
-    for (const auto& row : arr) {
-        if (!row.empty() && row[0] % 3 == 0) {
-            newArr.push_back(vector<int>(row.size(), 0));
+int** insertZeroRows(int** arr, int& n, int m) {
+    int new_rows_count = 0;
+    for (int i = 0; i < n; ++i) {
+        if (arr[i][0] % 3 == 0) {
+            new_rows_count++;
         }
-        newArr.push_back(row);
     }
-    arr = newArr;
-}
+    
     int n=0, m=0;
     cout << "Введите размеры массива (n m): ";
     cin >> n >> m;
